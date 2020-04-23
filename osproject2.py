0c28891245e6217340c242108b73c41969d29830
cesarvilla05 <55373706+cesarvilla05@users.noreply.github.com>
2020-04-22 22:02:44 -0500

    Add files via upload

---
+++ osproject2.py
@@ -0,0 +1,13 @@
+#This is going to be the simulator of a virtual memory page replacements
+#algotythems.
+#Cesar Villanueva- Vazquez  and Nick Mullen
+
+import sys
+import os
+randnumstring = []
+framenum = []
+#ref string arrar, and ref frame array
+
+class stringArray():
+    global randnumstring
+    global framenum
