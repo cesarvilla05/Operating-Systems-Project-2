f8d329dccfd7950777a6819e603c7aa745848099
cesarvilla05 <55373706+cesarvilla05@users.noreply.github.com>
2020-04-23 22:44:39 -0500

    FIFO
    did a little work but still not seeing proper printing

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import osproject2
 
 myPrj = osproject2.StringArray()
+myPrj2 = osproject2.fifoMethod()
+
+
 output = {}
+output2 = {}
 
 output = myPrj.run()
+output2 = myPrj2.run()
 
+print("here i am MAIN")
--- osproject2.py
+++ osproject2.py
@@ -1,17 +1,18 @@
 #This is going to be the simulator of a virtual memory page replacements
 #algotythems using FIFO.
 #Cesar Villanueva- Vazquez  and Nick Mullen
-
+import Queue as queue
 import sys
 import os
 import random
 import array
-import queue
 
 #Global Variables
 randNumString = []
 frameNum = []
 cycles = 30
+
+
 
 numberOfPages = 100
 #ref string arrar, and ref frame array
@@ -28,33 +29,29 @@ class StringArray():
             ranPageNumber = random.randint(0,49)
             randNumString.append(ranPageNumber)
 
+
 class fifoMethod():
     global randNumString
     global frameNum
 
-    def __init__(self, cycle, target, pageFault ):
-
-        self.currCycle = cycle
-        self.currTarget = target
-        self.fifoPageFault = pageFault
-
     def run(self):
 
         currPage = 0
-        self.fifoPageFault = 0
+        pageFaults = []*30  #keep track of all page faults in 30 cycles
+        
         q = queue.Queue()
 
         for i in range(cycles - 1):                 #we are running each method 30 times
             virtuMemoryFrames = [-1]*(i + 1)          #to maintain only i amount if frames
             found = False                           #flag to exit while loop
-                                      #helps run through virtuMemoryFrames
-
+            self.fifoPageFault = 0                                            #helps run through virtuMemoryFrames
 
             for j in range(len(randNumString) - 1):                         # Start going through 100 integer string
                 indexJ = 0
 
-                while found != True or indexJ >= len(virtuMemoryFrames):    #we can only go through the current amount
-                    print("hello")                                                     # of frms
+                for k in range(len(virtuMemoryFrames)):    #we can only go through the current amount
+                    foundFlag = 0
+                                                                              # of frms
                     if virtuMemoryFrames[indexJ] == -1:                      #initialize the Virtual Memory List
                         virtuMemoryFrames[j] = randNumString[currPage]
                         q.put(indexJ)
@@ -72,6 +69,80 @@ class fifoMethod():
 
                     else:
                         indexJ += 1
+                        self.fifoPageFault +=1
+            
+            pageFaults[i] = self.fifoPageFault    
+            #print(self.fifoPageFault, "current cycle", i)
+####################################################################################################################
+###############################################OPTIMAL###############################################
+# Optimal Page Replacement Algorithm
+# def __optimal():
+#     global a, n, m
+#     x = 0
+#     page_faults = 0
+#     page = []
+#     FREE = -1
+#     for i in range(m):
+#         page.append(FREE)
+#
+#     for i in range(n):
+#         flag = 0
+#         for j in range(m):
+#             if (page[j] == a[i]):
+#                 flag = 1
+#                 break
+#
+#         if flag == 0:
+#             # look for an empty one
+#             faulted = False
+#             new_slot = FREE
+#             for q in range(m):
+#                 if page[q] == FREE:
+#                     faulted = True
+#                     new_slot = q
+#
+#             if not faulted:
+#                 # find next use farthest in future
+#                 max_future = 0
+#                 max_future_q = FREE
+#                 for q in range(m):
+#                     if page[q] != FREE:
+#                         found = False
+#                         for ii in range(i, n):
+#                             if a[ii] == page[q]:
+#                                 found = True
+#                                 if ii > max_future:
+#                                     # print "\n\tFound what will be used last: a[%d] = %d" % (ii, a[ii]),
+#                                     max_future = ii
+#                                     max_future_q = q
+#
+#                                 break
+#
+#                         if not found:
+#                             # print "\n\t%d isn't used again." % (page[q]),
+#                             max_future_q = q
+#                             break
+#
+#                 faulted = True
+#                 new_slot = max_future_q
+#
+#             page_faults += 1
+#             page[new_slot] = a[i]
+#             print
+#             "\n%d ->" % (a[i]),
+#             for j in range(m):
+#                 if page[j] != FREE:
+#                     print
+#                     page[j],
+#                 else:
+#                     print
+#                     "-",
+#         else:
+#             print
+#             "\n%d -> No Page Fault" % (a[i]),
+#
+#     print
+#     "\n Total page faults : %d." % (page_faults)
 
 
 
