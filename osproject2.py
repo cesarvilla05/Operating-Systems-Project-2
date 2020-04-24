18020850cd08757fcceca31a4e46c1c0a857c759
cesarvilla05 <55373706+cesarvilla05@users.noreply.github.com>
2020-04-23 23:29:48 -0500

    FIFO ready
    I think the fifo is ready to use. the reason it wasn't accepting queue is because it needs to see import Queue as queue, rather than just import queue

--- osproject2.py
+++ osproject2.py
@@ -36,43 +36,39 @@ class fifoMethod():
 
     def run(self):
 
-        currPage = 0
+        
         pageFaults = []*30  #keep track of all page faults in 30 cycles
         
         q = queue.Queue()
 
-        for i in range(cycles - 1):                 #we are running each method 30 times
+        for i in range(cycles):                 #we are running each method 30 times
             virtuMemoryFrames = [-1]*(i + 1)          #to maintain only i amount if frames
-            found = False                           #flag to exit while loop
+            indexJ = -1
+            found = False
+            currPage = 0                           #flag to exit while loop
             self.fifoPageFault = 0                                            #helps run through virtuMemoryFrames
 
             for j in range(len(randNumString) - 1):                         # Start going through 100 integer string
-                indexJ = 0
-
+                foundFlag = 0
                 for k in range(len(virtuMemoryFrames)):    #we can only go through the current amount
-                    foundFlag = 0
-                                                                              # of frms
-                    if virtuMemoryFrames[indexJ] == -1:                      #initialize the Virtual Memory List
-                        virtuMemoryFrames[j] = randNumString[currPage]
-                        q.put(indexJ)
-                        self.fifoPageFault += 1
-                        currPage += 1
-                        found = True
-
-                    elif randNumString[currPage] == virtuMemoryFrames[indexJ]:#if current page is found go to next
+                                                           # of frms
+                    if randNumString[currPage] == virtuMemoryFrames[k]:#if current page is found go to next
                         currPage += 1                                          #next page and extit current while loop
-                        found = True
+                        foundFlag = 1
+                        break
 
-                    elif indexJ == len(virtuMemoryFrames) - 1:                #make function for when it end ofMemframes
-                        virtuMemoryFrames[q.get()] = randNumString[currPage]
-                        self.fifoPageFault += 1
-
-                    else:
-                        indexJ += 1
-                        self.fifoPageFault +=1
-            
-            pageFaults[i] = self.fifoPageFault    
-            #print(self.fifoPageFault, "current cycle", i)
+                if foundFlag == 0:                      #initialize the Virtual Memory List
+                    indexJ = (indexJ + 1) % len(virtuMemoryFrames)
+                    virtuMemoryFrames[indexJ] = randNumString[currPage]
+                    q.put(indexJ)
+                    self.fifoPageFault += 1
+                    currPage += 1
+                    found = True
+                                
+            pageFaults.append(self.fifoPageFault)    
+            print("\n")
+            print('cycle', i + 1)
+        print('FIFO Page faults', pageFaults)
 ####################################################################################################################
 ###############################################OPTIMAL###############################################
 # Optimal Page Replacement Algorithm
